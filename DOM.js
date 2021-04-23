080f721e52220fbd748f097689b4155354598809
Emilian Kasemi <60050952+theringsofsaturn@users.noreply.github.com>
2021-04-23 15:41:52 +0200

    initial commit

---
+++ DOM.js
@@ -0,0 +1,69 @@
+// Get element with ID "container" from the page
+let container = document.getElementById('container');
+
+// Get every "td" from the page
+let td = document.querySelectorAll("td")
+
+// let table = document.getElementById("table"), 
+//     tr = table.getElementsByTagName("tr"),
+//     td = tr.getElementsByTagName("td");
+
+// Create a cycle that prints the text inside every td of the page
+let textTD = document.querySelectorAll("td")
+textTD.innerText = "Changing the text"
+
+// Write a function to change the heading of the page
+
+function changeHeading() {
+    let reference = document.getElementById("h1").innerHTML = "New text!";
+}
+// Write a function to add an extra row to the table
+
+function addRow() {
+    // Find a <table> element with id="myTable":
+    let table = document.getElementById("myTable");
+    // Create an empty <tr> element and add it to the 1st position of the table:
+    let row = table.insertRow(0);
+  }
+// Write a function to add the class "test" to each row in the table
+
+for (let i = 0; i < tr.length; i++) {
+    let text = tr[i].innerText;
+    //check for your target text
+    if (text === "classText") {
+      //add your class to the element containing this text
+      tr[i].classList.add("classText");
+    }
+  }
+// Write a function to add a red background to every link in the page
+function changeBodyBg(color){
+    document.links.style.background = color;
+}
+// Console log "Page loaded" when the page is correctly loaded
+window.onload = function() {
+    console.log("Page loaded")
+ };
+// Write a function to add new items to a UL
+
+function createNewItem(name) {
+    let li = document.createElement('li');
+    li.textContent = name;
+    return li;
+}
+// get the ul#menu
+const menu = document.querySelector('#menu');
+// add menu item
+menu.appendChild(createNewItem('Home'));
+menu.appendChild(createNewItem('Services'));
+menu.appendChild(createNewItem('About Us'));
+// Write a function to empty a list
+function emptyList(){
+    document.getElementById("Ul").innerHTML = "";
+}
+
+// or a specific element
+
+function removeItem(item){
+    var itemToRemove = document.getElementById(item);
+    itemToRemove.parentNode.removeChild(itemToRemove);
+}
---
+++ exercises.js
@@ -0,0 +1,100 @@
+// Given variable x = "John" and y = "Doe", write on the console log "John <> Doe"
+x = "John"
+y = "Doe"
+console.log("John <> Doe")
+
+// Create an object with properties such name, surname, email
+let object = {
+    name: "Emilian",
+    surname: "Kasemi",
+    email: "emiliankasemi@gmail.com"
+}
+
+//  Delete Email from the previously created object
+
+delete object.email
+console.log(object)
+
+// Create an array with 10 strings in it
+
+let array = ["Mercedes", "Audi", "Volswagen", "BMW", "Toyota", "Rover", "Mazda", "Fiat", "Ferrari", "Renault"]
+
+// Print in the console every string in the previous array
+
+for (let i = 0; i < array.length; i++) {
+    console.log(array[i])
+}
+
+// Create an array with 100 random numbers in it
+
+let randomNumArr = [];
+for (let i = 0, n = 100; i < n; i++) {
+    randomNumArr.push(Math.round(Math.random() * n))
+}
+console.log(randomNumArr);
+
+// Wrote a function to get the MAX and the MIN from the previously created array
+
+// max
+function getMaxOfArray(randomNumArr) {
+    return Math.max.apply(null, randomNumArr);
+}
+
+console.log(randomNumArr)
+
+// min
+function getMinOfArray(randomNumArr) {
+    return Math.min.apply(null, randomNumArr);
+}
+
+console.log(randomNumArr)
+
+// Create an array of arrays, in which every array has 10 random numbers
+
+let firstArray = [];
+for(let i = 0; i < 4; i++){
+     let secondArray = [];
+     for(let randomNum = 0; randomNum < 10; randomNum++)
+          secondArray.push(Math.floor(Math.random() * 10));
+     firstArray.push(secondArray);
+}
+console.log(firstArray)
+
+// Create a function that gets 2 arrays and returns the longest one
+var longest = arr.reduce(
+    function (a, b) {
+        return a.length > b.length ? a : b;
+    }
+);
+
+// Create a function that gets 2 arrays of numbers and returns the one with the higher sum of values
+
+// returns minimum and maximum values of an array
+function minMax (arr) {
+    return [ Math.min(...arr), Math.max(...arr) ]
+  }
+  
+  // sum numbers in a range
+  function sumRange (min, max) {
+    let sum = 0
+  
+    for (let i = min; i <= max; i++) {
+      sum += i
+    }
+  
+    return sum
+  }
+  
+  function sumAll(arr) {
+    const [min, max] = minMax(arr)
+    const sum = sumRange(min, max)
+  
+    return sum
+  }
+  
+  // alternative one line solution
+  const sumAll = arr => sumRange(...minMax(arr))
+  
+  sumAll([1, 4])
+
+
